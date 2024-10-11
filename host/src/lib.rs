dc7aaba55f54095bffa243df5bbda0b8c1747cb3
Joel Dice <joel.dice@fermyon.com>
2024-10-11 15:53:26 -0600

    snapshot
    Signed-off-by: Joel Dice <joel.dice@fermyon.com>

--- host/src/lib.rs
+++ host/src/lib.rs
@@ -1,33 +1,46 @@
 #![deny(warnings)]
+#![feature(test)]
+
+extern crate test;
 
 #[cfg(test)]
 mod tests {
     use {
         anyhow::{anyhow, bail, Result},
-        futures::future::TryFutureExt,
+        futures::future,
         http_body_util::{BodyExt, Full},
-        hyper::{body::Bytes, Request},
+        hyper::{body::Bytes, Request, Response},
         rand::{rngs::StdRng, Rng, SeedableRng},
-        std::{iter, sync::Once},
-        tokio::{fs, process::Command, sync::oneshot, sync::OnceCell, task},
+        std::{cell::RefCell, future::Future, hint, iter, rc::Rc, sync::Once},
+        tempfile::TempDir,
+        test::Bencher,
+        tokio::{
+            fs,
+            process::Command,
+            runtime::Runtime,
+            sync::oneshot,
+            sync::OnceCell,
+            task::{self, JoinHandle},
+        },
         wasmtime::{
             component::{Component, Linker, ResourceTable},
-            Config, Engine, Store,
+            Config, Engine, InstanceAllocationStrategy, PoolingAllocationConfig, Store,
         },
         wasmtime_wasi::{DirPerms, FilePerms, WasiCtx, WasiCtxBuilder, WasiView},
         wasmtime_wasi_http::{
-            bindings::{http::types::Scheme, LinkOptions, Proxy},
+            bindings::{http::types::Scheme, LinkOptions, Proxy, ProxyPre},
+            body::HyperOutgoingBody,
             WasiHttpCtx, WasiHttpView,
         },
     };
 
-    struct Ctx {
+    struct Context {
         table: ResourceTable,
         wasi: WasiCtx,
         wasi_http: WasiHttpCtx,
     }
 
-    impl WasiView for Ctx {
+    impl WasiView for Context {
         fn table(&mut self) -> &mut ResourceTable {
             &mut self.table
         }
@@ -36,7 +49,7 @@ mod tests {
         }
     }
 
-    impl WasiHttpView for Ctx {
+    impl WasiHttpView for Context {
         fn table(&mut self) -> &mut ResourceTable {
             &mut self.table
         }
@@ -59,17 +72,57 @@ mod tests {
         }
     }
 
-    async fn test(component: &[u8], uri: &str) -> Result<()> {
-        static ONCE: Once = Once::new();
-        ONCE.call_once(pretty_env_logger::init);
+    struct Content {
+        first_part: &'static str,
+        content: String,
+        directory: TempDir,
+    }
 
+    async fn make_content() -> Result<Content> {
+        let first_part = "'Twas brillig, and the slithy toves\
+                          Did gyre and gimble in the wabe:\
+                          All mimsy were the borogoves,\
+                          And the mome raths outgrabe.\n\n";
+
+        let directory = tempfile::tempdir()?;
+        let mut rng = StdRng::seed_from_u64(42);
+        let file_count = 4;
+        let file_size = 1024 * 1024;
+        let mut content = first_part.to_string();
+        for i in 0..file_count {
+            let file_content = iter::repeat_with(|| rng.gen_range('a'..='z'))
+                .take(file_size)
+                .collect::<String>();
+            fs::write(
+                directory.path().join(format!("{i}")),
+                file_content.as_bytes(),
+            )
+            .await?;
+            content.push_str(&file_content);
+        }
+
+        Ok(Content {
+            first_part,
+            content,
+            directory,
+        })
+    }
+
+    struct State {
+        engine: Engine,
+        proxy_pre: ProxyPre<Context>,
+        content: Content,
+    }
+
+    async fn make_state() -> Result<State> {
         let mut config = Config::new();
-        config.wasm_component_model(true);
         config.async_support(true);
+        config.wasm_component_model(true);
+        config.allocation_strategy(InstanceAllocationStrategy::Pooling(
+            PoolingAllocationConfig::default(),
+        ));
 
         let engine = Engine::new(&config)?;
-
-        let component = Component::new(&engine, component)?;
 
         let mut linker = Linker::new(&engine);
 
@@ -80,108 +133,256 @@ mod tests {
             options
         })?;
 
-        let first_part = "'Twas brillig, and the slithy toves\
-                          Did gyre and gimble in the wabe:\
-                          All mimsy were the borogoves,\
-                          And the mome raths outgrabe.\n\n";
+        let component = Component::new(&engine, build_component("guest").await?)?;
 
-        let temp_dir = tempfile::tempdir()?;
-        let mut rng = StdRng::seed_from_u64(42);
-        let file_count = 4;
-        let file_size = 1024 * 1024;
-        let mut content = first_part.to_string();
-        for i in 0..file_count {
-            let file_content = iter::repeat_with(|| rng.gen_range('a'..='z'))
-                .take(file_size)
-                .collect::<String>();
-            fs::write(
-                temp_dir.path().join(format!("{i}")),
-                file_content.as_bytes(),
-            )
-            .await?;
-            content.push_str(&file_content);
+        let proxy_pre = ProxyPre::new(linker.instantiate_pre(&component)?)?;
+
+        let content = make_content().await?;
+
+        Ok(State {
+            engine,
+            proxy_pre,
+            content,
+        })
+    }
+
+    async fn state() -> &'static State {
+        static INIT_LOGGER: Once = Once::new();
+        INIT_LOGGER.call_once(pretty_env_logger::init);
+
+        static STATE: OnceCell<State> = OnceCell::const_new();
+        STATE
+            .get_or_init(|| async { make_state().await.unwrap() })
+            .await
+    }
+
+    fn make_context(state: &State) -> Result<Context> {
+        Ok(Context {
+            table: ResourceTable::new(),
+            wasi: WasiCtxBuilder::new()
+                .inherit_stdio()
+                .preopened_dir(
+                    state.content.directory.path(),
+                    ".",
+                    DirPerms::READ,
+                    FilePerms::READ,
+                )?
+                .build(),
+            wasi_http: WasiHttpCtx::new(),
+        })
+    }
+
+    fn make_store(state: &State, context: Context) -> Result<Store<Context>> {
+        Ok(Store::new(&state.engine, context))
+    }
+
+    #[derive(Copy, Clone)]
+    enum Strategy {
+        Stream,
+        Append,
+        StreamThenAppend,
+    }
+
+    impl Strategy {
+        fn uri(&self) -> &'static str {
+            match self {
+                Self::Stream => "http://localhost/stream",
+                Self::Append => "http://localhost/append",
+                Self::StreamThenAppend => "http://localhost/stream-then-append",
+            }
         }
+    }
 
-        let mut store = Store::new(
-            &engine,
-            Ctx {
-                table: ResourceTable::new(),
-                wasi: WasiCtxBuilder::new()
-                    .inherit_stdio()
-                    .preopened_dir(temp_dir.path(), ".", DirPerms::READ, FilePerms::READ)?
-                    .build(),
-                wasi_http: WasiHttpCtx::new(),
-            },
-        );
+    async fn run(
+        state: &State,
+        context: Context,
+        strategy: Strategy,
+    ) -> Result<(
+        JoinHandle<Result<(Store<Context>, Proxy)>>,
+        Response<HyperOutgoingBody>,
+    )> {
+        let mut store = make_store(state, context)?;
+        let proxy = state.proxy_pre.instantiate_async(&mut store).await?;
+        run_instance(state, store, proxy, strategy).await
+    }
 
+    async fn run_instance(
+        state: &State,
+        mut store: Store<Context>,
+        proxy: Proxy,
+        strategy: Strategy,
+    ) -> Result<(
+        JoinHandle<Result<(Store<Context>, Proxy)>>,
+        Response<HyperOutgoingBody>,
+    )> {
         let request = store.data_mut().new_incoming_request(
             Scheme::Http,
             Request::builder()
-                .uri(format!("http://localhost{uri}"))
-                .header("content-length", first_part.len().to_string())
+                .uri(strategy.uri())
+                .header("content-length", state.content.first_part.len().to_string())
                 .body(
-                    Full::new(Bytes::from_static(first_part.as_bytes()))
+                    Full::new(Bytes::from_static(state.content.first_part.as_bytes()))
                         .map_err(|_| unreachable!()),
                 )?,
         )?;
         let (tx, rx) = oneshot::channel();
 
         let response_out = store.data_mut().new_response_outparam(tx)?;
-        let proxy = Proxy::instantiate_async(&mut store, &component, &linker).await?;
 
-        let task = task::spawn(
-            async move {
-                proxy
-                    .wasi_http_incoming_handler()
-                    .call_handle(&mut store, request, response_out)
-                    .await?;
+        let task = task::spawn(async move {
+            proxy
+                .wasi_http_incoming_handler()
+                .call_handle(&mut store, request, response_out)
+                .await?;
 
-                Ok(())
-            }
-            .map_err(|e| {
-                eprintln!("error calling incoming handler: {e:?}");
-                e
-            }),
-        );
+            Ok((store, proxy))
+        });
 
-        let response = match rx.await {
-            Ok(Ok(response)) => Ok(response),
+        match rx.await {
+            Ok(Ok(response)) => Ok((task, response)),
             Ok(Err(e)) => Err(anyhow::Error::from(e)),
             Err(_) => {
                 let e = match task.await {
-                    Ok(r) => {
-                        r.expect_err("if the receiver has an error, the task must have failed")
-                    }
+                    Ok(r) => r
+                        .map(drop)
+                        .expect_err("if the receiver has an error, the task must have failed"),
                     Err(e) => anyhow::Error::from(e),
                 };
                 bail!("guest never invoked `response-outparam::set` method: {e:?}")
             }
-        }?;
-
-        assert!(content.as_bytes() == response.into_body().collect().await?.to_bytes());
-
-        Ok(())
+        }
     }
 
-    async fn guest() -> &'static [u8] {
-        static ONCE: OnceCell<Vec<u8>> = OnceCell::const_new();
-        &ONCE
-            .get_or_init(|| async { build_component("guest").await.unwrap() })
-            .await
+    async fn test(strategy: Strategy) -> Result<()> {
+        let state = state().await;
+        let (_, response) = run(state, make_context(state)?, strategy).await?;
+        assert!(
+            state.content.content.as_bytes() == read(response, strategy, future::ok(())).await?
+        );
+        Ok(())
     }
 
     #[tokio::test(flavor = "multi_thread")]
     async fn stream() -> Result<()> {
-        test(guest().await, "/stream").await
+        test(Strategy::Stream).await
     }
 
     #[tokio::test(flavor = "multi_thread")]
     async fn append() -> Result<()> {
-        test(guest().await, "/append").await
+        test(Strategy::Append).await
     }
 
     #[tokio::test(flavor = "multi_thread")]
     async fn stream_then_append() -> Result<()> {
-        test(guest().await, "/stream-then-append").await
+        test(Strategy::StreamThenAppend).await
+    }
+
+    async fn read(
+        response: Response<HyperOutgoingBody>,
+        strategy: Strategy,
+        recycle: impl Future<Output = Result<()>>,
+    ) -> Result<Bytes> {
+        let mut recycle = Some(recycle);
+
+        if let Strategy::Append = strategy {
+            recycle.take().unwrap().await?;
+        }
+
+        let bytes = response.into_body().collect().await?.to_bytes();
+
+        if let Strategy::Stream | Strategy::StreamThenAppend = strategy {
+            recycle.take().unwrap().await?;
+        }
+
+        Ok(bytes)
+    }
+
+    fn run_without_reuse(bencher: &mut Bencher, strategy: Strategy) {
+        let runtime = Runtime::new().unwrap();
+
+        let state = runtime.block_on(state());
+        let context = Rc::new(RefCell::new(Some(make_context(state).unwrap())));
+
+        let run = move || {
+            let context = context.clone();
+            async move {
+                let old_context = context.borrow_mut().take().unwrap();
+
+                let (task, response) = run(state, old_context, strategy).await?;
+
+                hint::black_box(
+                    read(response, strategy, async move {
+                        let mut ctx = task.await??.0.into_data();
+                        ctx.table = ResourceTable::new();
+                        *context.borrow_mut() = Some(ctx);
+                        Ok(())
+                    })
+                    .await?,
+                );
+
+                Ok::<_, anyhow::Error>(())
+            }
+        };
+
+        bencher.iter(|| runtime.block_on(run()).unwrap());
+    }
+
+    fn run_with_reuse(bencher: &mut Bencher, strategy: Strategy) {
+        let runtime = Runtime::new().unwrap();
+
+        let state = runtime.block_on(state());
+
+        let mut store = make_store(state, make_context(state).unwrap()).unwrap();
+        let proxy = Rc::new(RefCell::new(Some(
+            runtime
+                .block_on(state.proxy_pre.instantiate_async(&mut store))
+                .unwrap(),
+        )));
+        let store = Rc::new(RefCell::new(Some(store)));
+
+        let run = move || {
+            let store = store.clone();
+            let proxy = proxy.clone();
+            async move {
+                let old_store = store.borrow_mut().take().unwrap();
+                let old_proxy = proxy.borrow_mut().take().unwrap();
+
+                let (task, response) = run_instance(state, old_store, old_proxy, strategy).await?;
+
+                hint::black_box(
+                    read(response, strategy, async move {
+                        let (new_store, new_proxy) = task.await??;
+                        *store.borrow_mut() = Some(new_store);
+                        *proxy.borrow_mut() = Some(new_proxy);
+                        Ok(())
+                    })
+                    .await?,
+                );
+
+                Ok::<_, anyhow::Error>(())
+            }
+        };
+
+        bencher.iter(|| runtime.block_on(run()).unwrap());
+    }
+
+    #[bench]
+    fn stream_without_reuse(bencher: &mut Bencher) {
+        run_without_reuse(bencher, Strategy::Stream)
+    }
+
+    #[bench]
+    fn stream_with_reuse(bencher: &mut Bencher) {
+        run_with_reuse(bencher, Strategy::Stream)
+    }
+
+    #[bench]
+    fn append_without_reuse(bencher: &mut Bencher) {
+        run_without_reuse(bencher, Strategy::Append)
+    }
+
+    #[bench]
+    fn append_with_reuse(bencher: &mut Bencher) {
+        run_with_reuse(bencher, Strategy::Append)
     }
 }
